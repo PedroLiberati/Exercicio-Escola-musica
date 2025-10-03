d25e10cd962c03e824763524cf1d69cafe8a210f
PedroAlv <pedro.asouza@a.ucb.br>
2025-10-03 10:33:06 -0300

    Update EscolaMusica.java

--- EscolaMusica.java
+++ EscolaMusica.java
@@ -2,12 +2,23 @@ package com.mycompany.escolamusica;
 
 public class EscolaMusica {
     public static void main(String[] args) {
-        InstrumentoMusical violao = new ViolaoExercicio("Violão Clássico", "Madeira", 6);
-        InstrumentoMusical saxofone = new SaxofoneExercicio("Yamaha", "Latão", 23);
-        InstrumentoMusical piano = new PianoExercicio("Piano de Cauda", "Madeira", 88);
+        InstrumentoMusical[] banda = {
+            new PianoExercicio("Piano de Cauda", "Madeira", 88),
+            new ViolaoExercicio("Violão Clássico", "Madeira", 6),
+            new SaxofoneExercicio("Saxofone Yamaha", "Latão", 23),
+            new BateriaExercicio("Bateria Pearl", "Metal", 5)
+        };
 
-        violao.tocar();
-        saxofone.tocar();
-        piano.tocar();
+        System.out.println("== Apresentação da Orquestra ==\n");
+
+        System.out.println("Afinando os instrumentos...");
+        for (InstrumentoMusical instrumento : banda) {
+            instrumento.afinar();
+        }
+
+        System.out.println("\nAgora vamos ouvir a execução:");
+        for (InstrumentoMusical instrumento : banda) {
+            instrumento.tocar();
+        }
     }
 }
