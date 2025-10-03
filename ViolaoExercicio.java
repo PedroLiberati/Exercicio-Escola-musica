3ad2af08c828efa2e6bbe519957d806a17390734
PedroAlv <pedro.asouza@a.ucb.br>
2025-10-03 10:26:06 -0300

    Create ViolaoExercicio.java

---
+++ ViolaoExercicio.java
@@ -0,0 +1,28 @@
+package com.mycompany.escolamusica;
+
+public class ViolaoExercicio extends InstrumentoMusical {
+    private int numeroDeCordas;
+
+    public ViolaoExercicio(String material, int numeroDeCordas) {
+        super("Violão", material);
+        this.numeroDeCordas = numeroDeCordas;
+    }
+
+    @Override
+    public void afinar() {
+        System.out.println("Afinando o violão com " + numeroDeCordas + " cordas, feito de " + material + "...");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando o violão com " + numeroDeCordas + " cordas...");
+    }
+
+    public int getNumeroDeCordas() {
+        return numeroDeCordas;
+    }
+
+    public void setNumeroDeCordas(int numeroDeCordas) {
+        this.numeroDeCordas = numeroDeCordas;
+    }
+}
